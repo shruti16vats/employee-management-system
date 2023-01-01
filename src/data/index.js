823cd8853ba4d6f26c29a246e471e0b24c3401a4
SHRUTI <shrutivats16@gmail.com>
2023-01-01 20:47:49 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,28 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Shruti',
+    lastName: 'Vats',
+    email: 'shruti@example.com',
+    salary: '95000',
+    date: '2022-12-11'
+  },
+  {
+    id: 2,
+    firstName: 'Shubhanshu',
+    lastName: 'Shukla',
+    email: 'shubh@example.com',
+    salary: '180000',
+    date: '2022-12-17'
+  },
+  {
+    id: 3,
+    firstName: 'Megha',
+    lastName: 'Sharma',
+    email: 'anushka@example.com',
+    salary: '79000',
+    date: '2022-12-01'
+  }
+];
+
+export { employeesData };
